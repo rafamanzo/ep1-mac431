78488f6bc91341aabc82366d472f32d4ef34a7ae
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2012-09-27 08:50:22 -0300

    Fase 2 uma fração de segundo mais rápida que fase1

--- fase2.c
+++ fase2.c
@@ -1,19 +1,38 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-static long num_steps = 1000000;
 double step;
 
-void main (){
-  int i; double x, pi, sum = 0.0;
+void main (int argc, char *argv[]){
+  double step, pi, x, sum, partial_sum;
+  int num_threads, steps_per_thread, thread_num;
+  long num_steps, beginning, end, i;
+
+  num_threads = omp_get_num_procs()*omp_get_num_procs();
+  omp_set_num_threads(num_threads);
+
+  //partial_sum = malloc(num_threads*sizeof(double));
+
+  num_steps = atoi(argv[1]);
+  steps_per_thread = num_steps/num_threads;
   step = 1.0/(double) num_steps;
 
-  #pragma omp parallel for reduction(+:sum) private(x)
-  for (i=0;i < num_steps; i++){
-    x = (i+0.5)*step;
-    sum += 4.0/(1.0+x*x);
+  sum = 0.0;
+  #pragma omp parallel private(i, thread_num, x, partial_sum, beginning, end) reduction(+:sum)
+  {
+    thread_num = omp_get_thread_num();
+
+    partial_sum = 0.0;
+    beginning = (thread_num*steps_per_thread);
+    end = beginning + steps_per_thread;
+
+    for(i = beginning; i < end; i++){
+      x = (i+0.5)*step;
+      partial_sum += 4.0/(1+x*x);
+    }
+
+    sum += partial_sum;
   }
 
-  pi = step * sum;
-  printf("Pi = %g\n", pi);
+  printf("Pi = %g\n", step * sum);
 }
